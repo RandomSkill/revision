07c79476958af31ff5958d3584316bf1ede3b077
RandomSkill <simon.sabot@Gmail.com>
2023-12-21 14:31:34 +0100

    Style : change the style of the web site , center the content

--- module1/exo1/src/Components/Header/Header.jsx
+++ module1/exo1/src/Components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import "./Header.css"
+import pp from "./img/pp.jpg"
 const Header =(props)=>{
     return(
         <header>
+        <img src={pp} />
             <h1> Bienvenue !! {props.course}</h1>
+        
         </header>
     );
 }
